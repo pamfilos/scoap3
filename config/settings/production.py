e953f550805277b444c5d2fcc85f3c6f09b5ba75
pamfilos <pamfilosf@gmail.com>
2023-06-29 19:57:13 +0200

    s3: updates aws urls
    Signed-off-by: pamfilos <pamfilosf@gmail.com>

--- config/settings/production.py
+++ config/settings/production.py
@@ -87,8 +87,10 @@ AWS_S3_MAX_MEMORY_SIZE = env.int(
 # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
 AWS_S3_REGION_NAME = env("DJANGO_AWS_S3_REGION_NAME", default=None)
 # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#cloudfront
-AWS_S3_CUSTOM_DOMAIN = env("DJANGO_AWS_S3_CUSTOM_DOMAIN")
-AWS_S3_ENDPOINT_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}"
+AWS_S3_CUSTOM_DOMAIN = (
+    f'{env("DJANGO_AWS_STORAGE_BUCKET_NAME")}.{env("DJANGO_AWS_S3_CUSTOM_DOMAIN")}'
+)
+AWS_S3_ENDPOINT_URL = f'https://{env("DJANGO_AWS_S3_CUSTOM_DOMAIN")}'
 
 # STORAGE
 # ------------------------
