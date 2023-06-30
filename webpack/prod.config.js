ad0e0ee223023c2c3b106a9759138153e38ca152
pamfilos <pamfilosf@gmail.com>
2023-06-30 11:39:43 +0200

    ui: fix prod webpack assets
    Signed-off-by: pamfilos <pamfilosf@gmail.com>

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -2,7 +2,8 @@ const { merge } = require('webpack-merge');
 const commonConfig = require('./common.config');
 
 // This variable should mirror the one from config/settings/production.py
-const staticUrl = '/static/';
+// const staticUrl = '/static/';
+const staticUrl = 'https://scoap3-dev-backend.cern.ch/static/';
 
 module.exports = merge(commonConfig, {
   mode: 'production',
