1403fab1bca8284effa14bb94507a7c549075835
pamfilos <pamfilosf@gmail.com>
2023-07-06 16:03:44 +0200

    webpack: fix s3 static url
    Signed-off-by: pamfilos <pamfilosf@gmail.com>

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -2,8 +2,10 @@ const { merge } = require('webpack-merge');
 const commonConfig = require('./common.config');
 
 // This variable should mirror the one from config/settings/production.py
-// const staticUrl = '/static/';
-const staticUrl = 'https://scoap3-dev-backend.cern.ch/static/';
+const s3BucketName = process.env.DJANGO_AWS_STORAGE_BUCKET_NAME;
+const s3Url = `${s3BucketName}.${process.env.DJANGO_AWS_S3_CUSTOM_DOMAIN}`;
+
+const staticUrl = `https://${s3Url}/static/`;
 
 module.exports = merge(commonConfig, {
   mode: 'production',
