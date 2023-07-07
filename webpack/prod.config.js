c220f085bcdb30bbab2ffbd9e82e8ff8b7674149
pamfilos <pamfilosf@gmail.com>
2023-07-07 18:06:21 +0200

    ui: update production dockerfile, ci build
    Signed-off-by: pamfilos <pamfilosf@gmail.com>

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -3,9 +3,9 @@ const commonConfig = require('./common.config');
 
 // This variable should mirror the one from config/settings/production.py
 const s3BucketName = process.env.DJANGO_AWS_STORAGE_BUCKET_NAME;
-const s3Url = `${s3BucketName}.${process.env.DJANGO_AWS_S3_CUSTOM_DOMAIN}`;
+const s3Url = process.env.DJANGO_AWS_S3_CUSTOM_DOMAIN ? `${s3BucketName}.${process.env.DJANGO_AWS_S3_CUSTOM_DOMAIN}` : null;
 
-const staticUrl = `https://${s3Url}/static/`;
+const staticUrl =  s3Url ? `https://${s3Url}/static/` : '/static/';
 
 module.exports = merge(commonConfig, {
   mode: 'production',
