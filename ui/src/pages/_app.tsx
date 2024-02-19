9ddea9b335f391e67d7319691b01776b93516e5e
karolina-siemieniuk-morawska <55505399+karolina-siemieniuk-morawska@users.noreply.github.com>
2024-02-19 11:54:58 +0100

    ui: change fonts to self hosted google font

--- ui/src/pages/_app.tsx
+++ ui/src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { ConfigProvider } from "antd";
 import Head from "next/head";
 import NextNProgress from "nextjs-progressbar";
 import { MathJaxContext } from "better-react-mathjax";
+import { PT_Sans_Narrow } from "next/font/google";
 
 import "@/styles/globals.css";
 import theme from "../theme/themeConfig";
@@ -28,9 +29,21 @@ const config = {
   },
 };
 
+const font = PT_Sans_Narrow({
+  weight: ["400", "700"],
+  style: ["normal"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
 const App: React.FC<AppProps> = ({ Component, pageProps }) => (
   <MathJaxContext version={3} config={config}>
     <ConfigProvider theme={theme}>
+      <style jsx global>{`
+        html {
+          font-family: ${font.style.fontFamily};
+        }
+      `}</style>
       <Layout>
         <Head>
           <title>SCOAP3 Repository</title>
